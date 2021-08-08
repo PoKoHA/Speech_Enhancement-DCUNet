aeb47599cb99684115b0ca41a6706e15f59d1df2
park-cheol <ekqqqus5555@gamil.com>
2021-08-08 18:31:25 +0900

    nan으로 처리되는 문제 수정

--- metrics.py
+++ metrics.py
@@ -1,4 +1,5 @@
 from tqdm import tqdm
+import numpy as np
 
 import torch
 import torchaudio
@@ -10,8 +11,8 @@ def pesq_score(model, dataloader, criterion, args, N_FFT, HOP_LENGTH):
     test_pesq = 0.
     total_loss = 0
     istft = ISTFT(hop_length=HOP_LENGTH, n_fft=N_FFT).cuda(args.gpu)
-
     with torch.no_grad():
+        total_nan = 0
         for i, (mixed, target) in tqdm(enumerate(dataloader)):
             mixed = mixed.cuda(args.gpu)
             target = target.cuda(args.gpu)
@@ -19,6 +20,7 @@ def pesq_score(model, dataloader, criterion, args, N_FFT, HOP_LENGTH):
             # test loss 구하기WW
             pred_x = model(mixed) # time domain
             loss = criterion(args, N_FFT, HOP_LENGTH, mixed, pred_x, target)
+            # print(pred_x)
             total_loss += loss.item()
 
             # PESQ score 구하기
@@ -26,6 +28,8 @@ def pesq_score(model, dataloader, criterion, args, N_FFT, HOP_LENGTH):
             target = torch.squeeze(target, 1) # [batch, 1539, 214, 2]
 
             psq = 0.
+            nan = 0
+
             for idx in range(len(target)):
                 clean_x_16 = torchaudio.transforms.Resample(48000, 16000).cuda(args.gpu)(target[idx, :].view(1, -1))
                 pred_x_16 = torchaudio.transforms.Resample(48000, 16000).cuda(args.gpu)(pred_x[idx, :].view(1, -1))
@@ -33,12 +37,26 @@ def pesq_score(model, dataloader, criterion, args, N_FFT, HOP_LENGTH):
                 clean_x_16 = clean_x_16.cpu().numpy()
                 pred_x_16 = pred_x_16.detach().cpu().numpy()
 
-                psq += pesq(clean_x_16.flatten(), pred_x_16.flatten(), 16000)
+                # print("A", clean_x_16.flatten().size)
+                # print("B", pred_x_16.flatten().shape)
+                # print("i", i, "idx:", idx)
+                score = pesq(clean_x_16.flatten(), pred_x_16.flatten(), 16000)
 
-            psq /= len(target)
+                if np.isnan(score):
+                    nan += 1
+                    total_nan += 1
+                else:
+                    psq += score
+                # print("A", psq)
+
+            # print("B", len(target) - nan)
+            psq /= (len(target) - nan)
             test_pesq += psq
+            # print(test_pesq)
 
-        test_pesq /= len(dataloader)
+        # print(total_nan)
+        # print(len(dataloader) - total_nan)
+        test_pesq /= (len(dataloader) - total_nan)
         loss_avg = total_loss / len(dataloader)
 
     return test_pesq, loss_avg
